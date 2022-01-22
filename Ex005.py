3aafe02dd1361c8a6187799e480a240008997fbc
lleonardo1555@gmail.com <lleonardo1555@gmail.com>
2022-01-21 22:03:17 -0200

    ESCREVA UM PROGRAMA QUE INDIQUE O SEU SUCESSOR E SEU ANTECESSOR

---
+++ Ex005.py
@@ -0,0 +1,35 @@
+
+n = int(input('Digite um numero: '))
+a = n - 1
+s = n + 1
+print('Analisando o valor {}, seu antecessor e {} e o sucessor e {}'.format(n, a, s))
+
+
+n = int(input('Digite um numero: '))
+print('Analisando o valor {}, seu antecessor e {} e o sucessor e {}'.format(n, (n-1), (n+1)))
+
+
+n = int(input('Digite um numero'))
+print('Analisando o valor o seu e {}, e o seu sucessor e {}'.format(n, (n-1), (n+1)))
+
+
+
+n = int(input('Digite um numero novo'))
+print('Analisando os dados o seu sucessor e {}, e o antecessor e {}'.format(n, (n-1), (n+1)))
+
+
+n = int(input('Digite um novo numero novo'))
+
+print('Analisando o valor o seu sucessor e {}, e o seu antecessor e {}'.format(n, (n-1), (n+1)))
+
+
+
+
+
+
+
+
+
+
+
+
